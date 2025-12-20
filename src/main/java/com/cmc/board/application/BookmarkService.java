6fed5a43aa1081b8171c04d09c89358b9e870ddc
jihwankim128 <jihwan1535@nate.com>
2025-12-20 21:50:26 +0900

    feat: add board bookmark application layer

---
+++ src/main/java/com/cmc/board/application/BookmarkService.java
@@ -0,0 +1,28 @@
+package com.cmc.board.application;
+
+import com.cmc.board.application.port.in.CreateBookmarkUseCase;
+import com.cmc.board.application.port.out.ValidatePostPort;
+import com.cmc.board.domain.bookmark.Bookmark;
+import com.cmc.board.domain.bookmark.BookmarkRepository;
+import com.cmc.board.domain.exception.PostNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BookmarkService implements CreateBookmarkUseCase {
+
+    private final BookmarkRepository bookmarkRepository;
+    private final ValidatePostPort validatePostPort;
+
+    @Override
+    public void create(Long postId, Long userId) {
+        if (!validatePostPort.existsById(postId)) {
+            throw new PostNotFoundException();
+        }
+        Bookmark bookmark = Bookmark.create(postId, userId);
+        bookmarkRepository.save(bookmark);
+    }
+}
