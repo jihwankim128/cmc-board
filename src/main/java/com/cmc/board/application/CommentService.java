63afcf31b8a536a107a7b57b4b4ec27d0e3d7535
jihwankim128 <jihwan1535@nate.com>
2025-12-20 20:05:09 +0900

    feat: add board reply write application layer

--- src/main/java/com/cmc/board/application/CommentService.java
+++ src/main/java/com/cmc/board/application/CommentService.java
@@ -1,10 +1,13 @@
 package com.cmc.board.application;
 
 import com.cmc.board.application.port.in.WriteCommentUseCase;
+import com.cmc.board.application.port.in.WriteReplyUseCase;
+import com.cmc.board.application.port.in.command.ReplyCommentCommand;
 import com.cmc.board.application.port.in.command.WriteCommentCommand;
 import com.cmc.board.application.port.out.ValidatePostPort;
 import com.cmc.board.domain.comment.Comment;
 import com.cmc.board.domain.comment.CommentRepository;
+import com.cmc.board.domain.exception.CommentNotFoundException;
 import com.cmc.board.domain.exception.PostNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,7 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class CommentService implements WriteCommentUseCase {
+public class CommentService implements WriteCommentUseCase, WriteReplyUseCase {
     private final CommentRepository commentRepository;
     private final ValidatePostPort validatePostPort;
 
@@ -21,6 +24,16 @@ public class CommentService implements WriteCommentUseCase {
     public Long create(WriteCommentCommand command) {
         validatePost(command.postId());
         Comment saved = commentRepository.save(command.toComment());
+        return saved.getId();
+    }
+
+    @Override
+    public Long reply(ReplyCommentCommand command) {
+        validatePost(command.postId());
+        Comment parent = commentRepository.findById(command.parentCommentId())
+                .orElseThrow(CommentNotFoundException::new);
+        Comment reply = command.toReply(parent);
+        Comment saved = commentRepository.save(reply);
         return saved.getId();
     }
 
